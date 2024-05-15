45c9e627dd0cbc2d399f466d09dc3aa8025850d3
Tim <timothy.abramov16@ncf.edu>
2024-05-15 13:07:00 -0400

    a bit nicer graphs

--- GSSDashboard/server.R
+++ GSSDashboard/server.R
@@ -25,8 +25,12 @@ function(input, output, session) {
       print("Data adjusted for categorical:")
       str(plotData)
       
-      graph <- ggplot(plotData, aes(.data[[input$selectQuestionSingle]]))
-      graph <- graph + geom_bar() #bar plot
+      graph <- ggplot(plotData, aes(.data[[input$selectQuestionSingle]])) + 
+        geom_bar() + #bar plot
+        theme(panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        aes(stringr::str_wrap(.data[[input$selectQuestionSingle]], 15)) +
+        labs(x = "")
     }
     #if quantitative
     else if (input$selectQuestionSingle %in% c("hrs1", "sibs")){
@@ -34,22 +38,21 @@ function(input, output, session) {
       print("Data adjusted for quantitative")
       str(plotData[[input$selectQuestionSingle]])
       
-      graph <- ggplot(plotData, aes_(x = plotData[[input$selectQuestionSingle]]))
-      graph <- graph + geom_histogram() #histogram
+      graph <- ggplot(plotData, aes_(x = plotData[[input$selectQuestionSingle]])) + 
+        geom_histogram() #histogram
     }
     
-    #theme + title + axis adjustments
+    #other visual adjustments, applicable to all graphs
     graph <- graph + 
-      labs(title = paste(toupper(input$selectQuestionSingle), " Distribution"),
-           x = toupper(input$selectQuestionSingle),
-           y = "COUNT") + 
-      theme_minimal()
+      labs(title = paste(toupper(input$selectQuestionSingle), " Distribution")) +
+      theme(plot.title = element_text(size = 12, hjust = 0.5, face = "bold"))
     
     #our final output
     graph
   })
   
   output$compareQuestionPlot1 <- renderPlot({
+    
     #get the data for the selected question
     plotData <- data.frame(gss22[[input$selectQuestionCompare1]]) 
     names(plotData) <- input$selectQuestionCompare1
@@ -66,8 +69,12 @@ function(input, output, session) {
       print("Data adjusted for categorical:")
       str(plotData)
       
-      graph <- ggplot(plotData, aes(.data[[input$selectQuestionCompare1]]))
-      graph <- graph + geom_bar() #bar plot
+      graph <- ggplot(plotData, aes(.data[[input$selectQuestionCompare1]])) + 
+        geom_bar() + #bar plot
+        theme(panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        aes(stringr::str_wrap(.data[[input$selectQuestionCompare1]], 15)) +
+        labs(x = "")
     }
     #if quantitative
     else if (input$selectQuestionCompare1 %in% c("hrs1", "sibs")){
@@ -75,22 +82,21 @@ function(input, output, session) {
       print("Data adjusted for quantitative")
       str(plotData[[input$selectQuestionCompare1]])
       
-      graph <- ggplot(plotData, aes_(x = plotData[[input$selectQuestionCompare1]]))
-      graph <- graph + geom_histogram() #histogram
+      graph <- ggplot(plotData, aes_(x = plotData[[input$selectQuestionCompare1]])) + 
+        geom_histogram() #histogram
     }
     
-    #theme + title + axis adjustments
+    #other visual adjustments, applicable to all graphs
     graph <- graph + 
-      labs(title = paste(toupper(input$selectQuestionCompare1), " Distribution"),
-           x = toupper(input$selectQuestionCompare1),
-           y = "COUNT") + 
-      theme_minimal()
+      labs(title = paste(toupper(input$selectQuestionCompare1), " Distribution")) +
+      theme(plot.title = element_text(size = 12, hjust = 0.5, face = "bold"))
     
     #our final output
     graph
   })
   
   output$compareQuestionPlot2 <- renderPlot({
+    
     #get the data for the selected question
     plotData <- data.frame(gss22[[input$selectQuestionCompare2]]) 
     names(plotData) <- input$selectQuestionCompare2
@@ -107,8 +113,12 @@ function(input, output, session) {
       print("Data adjusted for categorical:")
       str(plotData)
       
-      graph <- ggplot(plotData, aes(.data[[input$selectQuestionCompare2]]))
-      graph <- graph + geom_bar() #bar plot
+      graph <- ggplot(plotData, aes(.data[[input$selectQuestionCompare2]])) + 
+        geom_bar() + #bar plot
+        theme(panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank()) +
+        aes(stringr::str_wrap(.data[[input$selectQuestionCompare2]], 15)) +
+        labs(x = "")
     }
     #if quantitative
     else if (input$selectQuestionCompare2 %in% c("hrs1", "sibs")){
@@ -116,16 +126,14 @@ function(input, output, session) {
       print("Data adjusted for quantitative")
       str(plotData[[input$selectQuestionCompare2]])
       
-      graph <- ggplot(plotData, aes_(x = plotData[[input$selectQuestionCompare2]]))
-      graph <- graph + geom_histogram() #histogram
+      graph <- ggplot(plotData, aes_(x = plotData[[input$selectQuestionCompare2]])) + 
+        geom_histogram() #histogram
     }
     
-    #theme + title + axis adjustments
+    #other visual adjustments, applicable to all graphs
     graph <- graph + 
-      labs(title = paste(toupper(input$selectQuestionCompare2), " Distribution"),
-           x = toupper(input$selectQuestionCompare2),
-           y = "COUNT") + 
-      theme_minimal()
+      labs(title = paste(toupper(input$selectQuestionCompare2), " Distribution")) +
+      theme(plot.title = element_text(size = 12, hjust = 0.5, face = "bold"))
     
     #our final output
     graph
