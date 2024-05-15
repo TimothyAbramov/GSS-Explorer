241a237519626feaa236090dc71d044a441fb3e0
Tim <timothy.abramov16@ncf.edu>
2024-05-15 09:28:57 -0400

    about page in

--- GSSDashboard/ui.R
+++ GSSDashboard/ui.R
@@ -54,7 +54,7 @@ dashboardPage(
       ),#tab2
       
       tabItem(tabName = "about",
-        HTML("Dashboard description here")
+        HTML("<p>The <b>General Social Survey (GSS)</b> is a wide-ranging survey conducted regularly in the <b>United States</b>, aiming to understand and track societal trends and attitudes. Covering topics from politics and religion to demographics and well-being, the GSS provides valuable insights into the changing fabric of American society. By asking a diverse range of questions to a representative sample of the population, the survey captures nuanced shifts in <b>beliefs, behaviors, and opinions</b> over time, making it a vital tool for <b>sociologists, policymakers, and researchers</b> alike.</p><br><p>Focusing on <b>2022 GSS data</b>, this dashboard serves as a powerful tool for uncovering insights into the social landscape of America. By visually presenting key metrics derived from the survey, it allows users to explore <b>correlations, patterns, and anomalies</b> within the data. Whether examining <b>demographic shifts, political affiliations, or societal attitudes</b>, dashboard provides a dynamic platform for gaining a deeper understanding of the complexities shaping American society.</p>")
       )#tab3
       
     )#tabItems
