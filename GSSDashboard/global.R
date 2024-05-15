73d1c3b01b3eea928c456db2536bf69a1f927e13
Tim <timothy.abramov16@ncf.edu>
2024-05-15 08:35:22 -0400

    selected vars for prototype

--- GSSDashboard/global.R
+++ GSSDashboard/global.R
@@ -5,6 +5,18 @@ library(gssrdoc)
 gss22 <- gss_get_yr(2022)
 
 #see how to figure out what kind of question it is
-labels <- attr(gss22$wrkstat, "labels")
-str(labels)
-labels_names<- attr(labels, "names")
+# labels <- attr(gss22$wrkstat, "labels")
+# str(labels)
+# labels_names<- attr(labels, "names")
+
+#selected questions to start off the thing:
+#wrkstat (categorical, k>2) Last week were you working full time, part time, going to school, keeping house, or what?
+str(gss22$wrkstat) #dbl + lbl
+#wrkslf (categorical, k = 2) (Are/were) you self employed or (do/did) you work for someone else?
+str(gss22$wrkslf) #dbl + lbl
+
+#hrs1 (quantitative) If working, full or part time: how many hours did you work last week, at all jobs?
+str(gss22$hrs1) #dbl+lbl
+#sibs (quantitative) How many brothers and sisters did you have? Please count those born alive, but no longer living, as well as those alive now. Also include stepbrothers and stepsisters, and children adopted by your parents.
+str(gss22$sibs) #dbl+lbl
+
